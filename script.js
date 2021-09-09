f36259233587051cf3891a2ba5aa169fa3521ccc
josh manansala <joshmanansalagsd@gmail.com>
2021-09-09 00:43:48 -0700

    fixed button logic

--- script.js
+++ script.js
@@ -6,21 +6,16 @@ function emailValidation() {
     const userInput = document.querySelector('.inputEmail').value;
     const regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
     const error = document.querySelector('.errorMessage');
-    const inputborder = document.querySelector('.inputEmail');
+    const inputBorder = document.querySelector('.inputEmail');
 
     if (regx.test(userInput) == true) {
-        error.className = "errorMessage";
-        inputborder.className = "inputEmail";
-        notifyButton.style.backgroundColor = '#3dd28d';
+        error.style.display = 'none';
+        inputBorder.style.borderColor = 'hsl(223, 100%, 88%)';
         alert('Thank you');
-        
     } else if (regx.test(userInput) !== true) {
-        error.className = "errorMessageShow";
-        inputborder.className = "inputEmailError";
-        notifyButton.style.backgroundColor = 'hsl(223, 87%, 63%)';
-    } else {
-       
-    }
+        alert('Sorry');
+        inputBorder.style.borderColor = 'hsl(354, 100%, 66%)';
+        error.style.display = 'block';
+    } 
 }
-
 notifyButton.addEventListener('click', emailValidation);
