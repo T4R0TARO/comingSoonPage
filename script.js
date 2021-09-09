3ace9523367b8c68e88bdfcda802b2fcf72be274
josh manansala <joshmanansalagsd@gmail.com>
2021-09-08 20:41:00 -0700

    mobile desktop complete button logic in development

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log('wah');
+
+const notifyButton = document.querySelector('.submitButton');
+
+function emailValidation() {
+    const userInput = document.querySelector('.inputEmail').value;
+    const regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
+    const error = document.querySelector('.errorMessage');
+    const inputborder = document.querySelector('.inputEmail');
+
+    if (regx.test(userInput) == true) {
+        error.className = "errorMessage";
+        inputborder.className = "inputEmail";
+        notifyButton.style.backgroundColor = '#3dd28d';
+        alert('Thank you');
+        
+    } else if  (regx.test(userInput) == false) {
+        error.className = "errorMessageShow";
+        inputborder.className = "inputEmailError";
+        notifyButton.style.backgroundColor = 'hsl(223, 87%, 63%)';
+    } else {
+        error.className = "errorMessage";
+        inputborder.className = "inputEmail";
+    }
+}
+
+notifyButton.addEventListener('click', emailValidation);
